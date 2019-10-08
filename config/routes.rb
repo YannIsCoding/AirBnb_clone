08b529b2db923c44f2cc635d7002ba75a7a036b1
Yann Parent <yann.parent4@gmail.com>
2019-10-08 15:07:04 +0200

    Basic shoidnex show create for flats

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # get 'flats/index'
+  # get 'flats/new'
+  # get 'flats/create'
+  resources :flats
   devise_for :users
-  root to: 'pages#home'
+  root to: 'flats#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'super flat',
+  address: '42 super street',
+  description: 'This is a really super flat',
+  price_per_night: '42',
+  number_of_guests: 2
+  )
+Flat.create!(
+  name: 'mega flat',
+  address: '42 mega street',
+  description: 'This is a really mega flat',
+  price_per_night: '42',
+  number_of_guests: 2
+  )
+Flat.create!(
+  name: 'giga flat',
+  address: '42 giga street',
+  description: 'This is a really giga flat',
+  price_per_night: '42',
+  number_of_guests: 2
+  )
+Flat.create!(
+  name: 'tetra flat',
+  address: '42 tetra street',
+  description: 'This is a really tetra flat',
+  price_per_night: '42',
+  number_of_guests: 2
+  )
+Flat.create!(
+  name: 'peta flat',
+  address: '42 peta street',
+  description: 'This is a really peta flat',
+  price_per_night: '42',
+  number_of_guests: 2
+  )
+
+puts 'Flats: IT IS DONE.'
+
+User.create!(
+  email: 'andy@lorem.epislom',
+  password: '123456'
+  )
+puts 'User: IT IS DONE'
