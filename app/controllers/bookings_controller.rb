a82b4342280d6f865035a1934ac4ca7b9d25228b
Yann Parent <yann.parent4@gmail.com>
2019-10-09 12:51:28 +0200

    booking model and controler

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,46 @@
+class BookingsController < ApplicationController
+  before_action :find_booking, only: [:show]
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @flat = Flat.find(params[:flat_id])
+    @booking.flat_id = @flat.id
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  # get 'flats/index'
-  # get 'flats/new'
-  # get 'flats/create'
+
+  resources :bookings
   resources :flats
+  resources :flats do
+    resources :bookings, only: [:index, :new, :create]
+  end
+  resources :users do
+    resources :bookings, only: [:index]
+  end
   devise_for :users
   root to: 'flats#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
